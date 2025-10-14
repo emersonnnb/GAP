77b1cacb1dcc70f22d69315194b6f8a644d99128
Emerson <emerson.barbosa@extreme.digital>
2025-10-14 00:17:06 -0300

    Implementação do Sidebar

--- src/app/components/menu/menu.ts
+++ src/app/components/menu/menu.ts
@@ -1,11 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, signal, WritableSignal } from '@angular/core';
+import { Router, RouterModule, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-menu',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, MatIconModule, MatToolbarModule, MatMenuModule, MatSidenavModule, MatListModule, RouterModule, MatButtonModule],
   templateUrl: './menu.html',
   styleUrl: './menu.scss'
 })
 export class Menu {
 
+  selectedMenu: WritableSignal<string> = signal('dashboard');
+  sidebarCollapsed: WritableSignal<boolean> = signal(false);
+  
+  menuItems: MenuItem[] = [
+    { label: 'Dashboard', icon: 'monitoring', link: 'dashboard' },
+    { label: 'Produtos', icon: 'lists', link: 'produtos' },
+    { label: 'Gestão', icon: 'group', link: 'gestao' },
+    { label: 'Financeiro', icon: 'currency_exchange', link: 'financeiro' },
+    { label: 'Configuração', icon: 'settings', link: 'configuracao' },
+  ];
+
+  selectMenu(link: string) {
+    this.selectedMenu.set(link);
+  }
+
+  selectedMenuLabel() {
+    const item = this.menuItems.find(m => m.link === this.selectedMenu());
+    return item?.label ?? '';
+  }
+
+  toggleSidebar() {
+    this.sidebarCollapsed.set(!this.sidebarCollapsed());
+  }
+
+}
+
+interface MenuItem {
+  label: string;
+  icon: string;
+  link: string;
 }
